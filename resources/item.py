3460d009c6270ba4c2f55d2c7c71cf5fe97b3fba
Matías Gutiérrez <matyuh@hotmail.com>
2021-10-11 14:13:15 -0300

    added jwt_optional to itemlist resource for displaying full information to logged users

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 class Item(Resource):
@@ -41,7 +41,7 @@ class Item(Resource):
         claims = get_jwt()
         if not claims['is_admin']:
             return {'message': 'Admin privilege required.'}, 401
-            
+
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
@@ -68,5 +68,13 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items],
+            'message': "More data available if you log in."
+        }, 200
